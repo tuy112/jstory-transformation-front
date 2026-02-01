76140dae750124a1b615b1e0331a737cedd2abb1
Kim Jae Hyeok <master_catch@naver.com>
2026-02-02 01:36:33 +0900

    fix : css 수정 및 분리 작업

--- src/index.js
+++ src/index.js
@@ -4,23 +4,26 @@ import App from './App';
 
 // style 모음
 // Common css
-import './static/css/common/common.css';
 import './static/css/common/reset.css';
+import './static/css/common/common.css';
+import './static/css/common/header.css';
+import './static/css/common/footer.css';
 import './static/css/common/gate.css';
 import './static/css/common/modal.css';
 
 // Jstory css
 import './static/css/main.css';
 import './static/css/sub.css';
-import './static/css/login.css';
 
 // Project css
-import './static/css/mobile/mobile.css';
 import './static/css/mountain/mountain.css';
 import './static/css/mountain/mountainModal.css';
 import './static/css/study/study.css';
 import './static/css/three/three.css';
 import './static/css/todo/todo.css';
+import './static/css/diet/diet.css';
+
+import './static/css/mobile/mobile.css';
 
 import { BrowserRouter } from 'react-router-dom';
 
