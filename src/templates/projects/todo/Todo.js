399a8a013bd540df2034d1765e07efc48d9430a0
Kim Jae Hyeok <master_catch@naver.com>
2026-02-02 01:43:51 +0900

    fix : 투두리스트 수정 작업

--- src/templates/projects/todo/Todo.js
+++ src/templates/projects/todo/Todo.js
@@ -1,20 +1,8 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
 import Header from "../../component/Header";
 import Footer from "../../component/Footer";
-import TilModal from "../../component/TilModal";
+import datas from "./data/data";
 
 const Todo = (() => {
-    const navigate = useNavigate();
-    const todos = [
-        {
-            id:"1",
-            title:"오라클 공부",
-            summary:"쿼리 짜는 연습",
-            date:"2025-04-02"
-        }
-    ]; // TIL 임시 데이터
 
     return (
         <div id="wrap">
@@ -24,10 +12,10 @@ const Todo = (() => {
                 <div className="todo-container" id="todoList">
                     <div className="record-container">
                         <div className="title-section">
-                            <h1>지금까지 쓴 TIL / WIL 모음</h1>
+                            <h1>TODOLIST</h1>
                             <div className="btn-group">
-                                <button className="add-btn" onClick={() => navigate("/til")}>TIL 추가</button>
-                                <button className="add-btn" onClick={() => navigate("/til")}>WIL 추가</button>
+                                <button className="add-btn">오늘 할 일 추가</button>
+                                <button className="add-btn">할 일 수정</button>
                             </div>
                         </div>
 
@@ -42,13 +30,13 @@ const Todo = (() => {
                                 </tr>
                             </thead>
                             <tbody className="todo-list">
-                                {todos.length > 0 ? (
-                                    todos.map((todo, index) => (
+                                {datas.length > 0 ? (
+                                    datas.map((data, index) => (
                                         <tr key={index}>
-                                            <td>{todo.id}</td>
-                                            <td>{todo.title}</td>
-                                            <td>{todo.summary}</td>
-                                            <td>{todo.date}</td>
+                                            <td>{data.id}</td>
+                                            <td>{data.todo}</td>
+                                            <td>{data.summary}</td>
+                                            <td>{data.status}</td>
                                             <td>
                                                 <button className="delete-btn">삭제</button>
                                             </td>
@@ -56,48 +44,7 @@ const Todo = (() => {
                                     ))
                                 ) : (
                                     <tr>
-                                        <td colSpan="5" style={{ textAlign: "center" }}>공부 기록이 없습니다. 공부좀 해..</td>
-                                    </tr>
-                                )}
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div className="record-container">
-                        <div className="title-section">
-                            <h1>운동 + 몸무게 기록</h1>
-                            <div className="btn-group">
-                                <button className="add-btn" onClick={() => navigate("/til")}>TIL 추가</button>
-                                <button className="add-btn" onClick={() => navigate("/til")}>WIL 추가</button>
-                            </div>
-                        </div>
-
-                        <table className="todo-table">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>제목</th>
-                                    <th>요약 내용</th>
-                                    <th>등록일</th>
-                                    <th>삭제</th>
-                                </tr>
-                            </thead>
-                            <tbody className="todo-list">
-                                {todos.length > 0 ? (
-                                    todos.map((todo, index) => (
-                                        <tr key={index}>
-                                            <td>{todo.id}</td>
-                                            <td>{todo.title}</td>
-                                            <td>{todo.summary}</td>
-                                            <td>{todo.date}</td>
-                                            <td>
-                                                <button className="delete-btn">삭제</button>
-                                            </td>
-                                        </tr>
-                                    ))
-                                ) : (
-                                    <tr>
-                                        <td colSpan="5" style={{ textAlign: "center" }}>공부 기록이 없습니다. 공부좀 해..</td>
+                                        <td colSpan="5" style={{ textAlign: "center" }}>오늘 할 일이 없습니다.</td>
                                     </tr>
                                 )}
                             </tbody>
---
+++ src/templates/projects/todo/data/data.js
@@ -0,0 +1,9 @@
+const datas = [
+    { no: 1, todo: "JAVA 및 SQL 공부", summary: "요약내용", status: "not-started" },
+    { no: 2, todo: "RMS 기록물 인수", summary: "요약내용", status: "done" },
+    { no: 3, todo: "RMS 기록물 보존", summary: "요약내용", status: "in-progress" },
+    { no: 4, todo: "RMS 기록물 평가", summary: "요약내용", status: "not-started" },
+    { no: 5, todo: "RMS 기록물 평가", summary: "요약내용", status: "not-started" },
+    { no: 6, todo: "스킬업 프로젝트 구축", summary: "요약내용", status: "in-progress" },
+    { no: 7, todo: "Jstory 고도화", summary: "요약내용", status: "in-progress" }
+]
