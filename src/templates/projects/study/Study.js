09e199b04b669856512595e874dafaa910c52176
Kim Jae Hyeok <master_catch@naver.com>
2026-02-02 07:49:18 +0900

    fix : 기존 study 페이지 삭제 및 새로운 study 초안 생성

---
+++ src/templates/projects/study/Study.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function study() {
+  return (
+    <div id='wrap'>
+        <div className="study-page">
+            <h1>Study Project Page</h1>
+            <p>This is the study project page.</p>
+        </div>
+    </div>
+  );
+}
+
+export default study;
